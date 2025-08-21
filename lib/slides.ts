7d619a4eb3ff78abfef3ed5d0d0bb5718ec144b4
tenova-ke <wangatracker@gmail.com>
2025-08-21 11:32:08 +0300

    Create slides.ts

---
+++ lib/slides.ts
@@ -0,0 +1,14 @@
+// lib/slides.ts
+const slides = [
+  "/view/Gradient_Text_WANGA.png (1).jpg",
+  "/view/Logo_Maker_WANGA.png (1).jpg",
+  "/view/Neon_Glitch_WANGA.png (2).jpg",
+  "/view/Pixel_Glitch_WANGA.png (1).jpg",
+  "/view/Pixel_Glitch_WANGA.png.jpg",
+  "/view/Text_Effect_WANGA.png.jpg",
+  "/view/image-19.jpg",
+  "/view/image-7.jpg",
+  "/view/image.jpg",
+];
+
+export default slides;
