08a3ff8af092e3969eb5e8178625534a7c2a160b
tenova-ke <wangatracker@gmail.com>
2025-08-21 10:28:57 +0300

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center py-6 bg-card text-sm text-text">
+      © {new Date().getFullYear()} Tenova. All rights reserved.
+    </footer>
+  );
+      }
