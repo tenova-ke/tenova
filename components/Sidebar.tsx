82bfdcac95d9655304dcf272589db7135b686640
tenova-ke <wangatracker@gmail.com>
2025-08-21 23:47:06 +0300

    Create Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-60 bg-slate-900 text-white min-h-screen p-4">
+      <h1 className="text-xl font-bold mb-6">Tevona</h1>
+      <ul className="space-y-3">
+        <li>System Info</li>
+        <li>Scraper</li>
+        <li>Code Playground</li>
+        <li>Audio</li>
+        <li>Movies</li>
+        <li>News</li>
+      </ul>
+    </aside>
+  );
+}
