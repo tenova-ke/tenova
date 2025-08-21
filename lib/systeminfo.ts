96abb14fecafbdfec80d7e569d381bde79ac13cb
tenova-ke <wangatracker@gmail.com>
2025-08-21 22:27:20 +0300

    Update systeminfo.ts

--- lib/systeminfo.ts
+++ lib/systeminfo.ts
@@ -1,1 +1,21 @@
+import si from "systeminformation";
+import publicIp from "public-ip";
+import geoip from "geoip-lite";
+import dayjs from "dayjs";
 
+export async function getSystemInfo() {
+  const cpu = await si.cpu();
+  const mem = await si.mem();
+  const os = await si.osInfo();
+  const ip = await publicIp.v4();
+  const geo = geoip.lookup(ip || "") || {};
+
+  return {
+    time: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+    os: os.distro,
+    cpu: cpu.brand,
+    totalMem: (mem.total / 1e9).toFixed(2) + " GB",
+    ip,
+    geo
+  };
+    }
