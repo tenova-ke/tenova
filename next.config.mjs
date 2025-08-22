4146914c726574c30091d91db877458c2a300fcb
tenova-ke <wangatracker@gmail.com>
2025-08-22 09:14:31 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,16 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "osx-temperature-sensor": false,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
