deadada3d1a329b3843a2743c1bbf9392dfe881f
tenova-ke <wangatracker@gmail.com>
2025-08-24 10:12:28 +0300

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="flex items-center justify-center py-6 bg-card text-sm text-text">
-      © {new Date().getFullYear()} Tenova. All rights reserved.
-    </footer>
-  );
-      }
